f50034ce6ca59be1620435b3ef6dc0f3fe57165e
Deven <masterdeven@gmail.com>
2021-05-16 23:58:27 -0400

    add revision cpp file

---
+++ basics.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  printf("Hello world\n");
+  int x;
+  // scanf("%d", &x);
+  // printf("Value of x is %d\n", x);
+
+  std:: cout << "/* Demo */" << '\n';
+  int y;
+  std::cin >> y;
+  std::cout << "this is the value of " << y << '\n';
+  std::cout << "this is just amazing" << '\n';
+
+  float a,b;
+  std::cin >> a >> b;
+  if (a > b)
+    std::cout << a/b << '\n';
+  else
+    std::cout << b/a << '\n';
+  return 0;
+}
