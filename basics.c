a3b7e329cc25125fbb61b7c2a9528ef96b251f0e
Deven <masterdeven@gmail.com>
2021-05-16 23:57:51 -0400

    add c basics file

---
+++ basics.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    printf("Hello world!\n");
+    int x;
+    scanf("%d", &x);
+    printf("Value of x is %d\n", x);
+    printf("All this feels super complicated\n");
+
+
+
+    return 0;
+}
